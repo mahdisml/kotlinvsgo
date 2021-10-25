d565cc8d0da1e46706bdf3af3f98fd1dbd65581d
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-10-25 23:37:03 +0330

    Update app.go

--- 1/app.go
+++ 1/app.go
@@ -1,1 +1,16 @@
-sml
+package main
+
+import "fmt"
+
+func main() {
+	var x []string = []string{"salam", "alo", "alo"}
+	x = append(x, "3")
+	for i := range x {
+		if i == 1 {
+			x = append(x[:1], x[2:]...)
+		}
+	}
+	for i, s := range x {
+		fmt.Println(i, ":", s)
+	}
+}
