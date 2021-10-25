75d5f25b340a2fc0a083033202ce36bd9cf2a5c5
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2021-10-25 23:37:18 +0330

    Update app.kt

--- 1/app.kt
+++ 1/app.kt
@@ -1,1 +1,13 @@
-sml
+fun main (){
+    val x : MutableList<String> = mutableListOf("salam", "alo", "alo")
+    x.add("3")
+    for ((i,v) in x.withIndex()){
+        if (i == 2){
+            x.remove(v)
+        }
+    }
+    for ((i,v) in x.withIndex()){
+        println("$i : $v")
+    }
+
+}
